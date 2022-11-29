48605ff8f74753228055b66d95c4c8f92c828121
Lee Seungwook <blue_notion@naver.com>
2022-11-29 23:24:01 +0900

    Login Page 1.0ver
    Fetch error : (

--- WebContent/web/main/src/App.js
+++ WebContent/web/main/src/App.js
@@ -1,13 +1,67 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+// import { Adsense } from '@ctrl/react-adsense';
 // import Slider from "react-slick";
 import './App.css';
+import Login from './components/Login';
+import Redirect from './components/Redirect';
+
+
+function Main() {
+
+  const dispatch = useDispatch();
+  const checkStory = useSelector((state) => state.checkStory);
+
+  useEffect(() => {
+    const cookieData = document.cookie.split(';');
+    for (let i = 0; i < cookieData.length; i++) {
+      if (cookieData[i].indexOf('planetter') > -1) {
+      } else {
+        dispatch({ type: 'CHANGE_CHECKSTORY', data: !checkStory });
+      } break;
+    };
+  }, []);
+
+  return (
+    <div className='main_background'>
+      {/* <ModalStory></ModalStory>
+      <Menu></Menu>
+      <NameOfPlanet></NameOfPlanet> */}
+      <h1 className='loging'><NavLink end to="/login"></NavLink></h1>
+    </div>
+  );
+};
 
 
 function App() {
+
+  const navigater = useNavigate();
+  const userID = useSelector((state) => state.userID);
+
+  useEffect(() => {
+    console.log(userID);
+    // Check user device.
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    } else {
+      alert("모바일 환경에 최적화 되어 있습니다.");
+    };
+    if (userID === null) {
+      navigater('/login');
+    } else {
+    };
+  }, [navigater, userID])
+
   return (
-    <div>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/*" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/redirect" element={<Redirect />}></Route>
+        {/* <Route path="/redirect2" element={<Redirect2 />}></Route> */}
+      </Routes>
+    </React.Fragment>
   );
 }
 
--- WebContent/web/main/src/store/store.js
+++ WebContent/web/main/src/store/store.js
@@ -3,10 +3,34 @@ import { legacy_createStore as createStore } from "redux";
 function reducer(state, action) {
     if (state === undefined) {
         return {
+            userID: null,
+            checkStory: true,
+            userData: {'nickname' : 'null', 'openDate' : null},
+            letterData: [],
+            isConditions: false,
+            isIndividual: false,
+
 
         };
     };
     const newState = { ...state };
+
+    if (action.type === 'CHANGE_USERID') {
+        newState.userID = action.data;
+    };
+
+    if (action.type === 'CHANGE_CHECKSTORY') {
+        newState.checkStory = action.data;
+    };
+    
+    if (action.type === 'CHANGE_ISCONDITIONS') {
+        newState.isConditions = action.data;
+    }; 
+
+    if (action.type === 'CHANGE_ISINDIVIDUAL') {
+        newState.isIndividual = action.data;
+    }; 
+
     return newState;
 };
 
