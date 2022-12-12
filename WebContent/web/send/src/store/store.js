c81f72547e77a51580df5a4b631fbd9a01107267
Lee Seungwook <blue_notion@naver.com>
2022-12-12 21:55:09 +0900

    working - send page
    - write func
    - sticker func

--- WebContent/web/send/src/store/store.js
+++ WebContent/web/send/src/store/store.js
@@ -3,14 +3,33 @@ import { legacy_createStore as createStore } from "redux";
 function reducer(state, action) {
     if (state === undefined) {
         return {
-            store: 'null'
- 
+            userID: null,
+            textLength: 0,
+            text: '',
+            stickerArray: [],
+            stickerNumber: 0
         };
     };
     const newState = { ...state };
 
-    if (action.type === 'CHANGE_STORE') {
-        newState.store = action.data;
+    if (action.type === 'ADD_USERID') {
+        newState.userID = action.data;
+    };
+
+    if (action.type === 'CHANGE_TEXTLENGTH') {
+        newState.textLength = action.data;
+    };
+
+    if (action.type === 'CHANGE_TEXT') {
+        newState.text = action.data;
+    };
+
+    if (action.type === 'CHANGE_STICKER') {
+        newState.stickerArray = action.data;
+    };
+
+    if (action.type === 'CHANGE_STICKER_NUMBER') {
+        newState.stickerNumber = action.data;
     };
 
     return newState;
