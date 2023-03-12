34ab07a0845c83c285dae9588477e805ffe0bcc9
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-03-12 22:07:26 +0900

    Revert "update_2023-03-12"

---
+++ WebContent/web/main/src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import './App.css';
+import Main from './components/Main';
+import Login from './components/Login';
+import Redirect from './components/Redirect';
+import Redirect2 from './components/Redirect2';
+import Send from './components/Send';
+
+function App() {
+  const navigater = useNavigate();
+  const dispatch = useDispatch();
+  const userID = useSelector((state) => state.userID);
+  const isFirstInfo = useSelector((state) => state.isFirstInfo);
+
+  console.log(userID);
+
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  function FirstInfo() {
+    return (
+      <React.Fragment>
+        <div className={isFirstInfo ? "isFirstIngo_wrap" : "isFirstIngo_fade"}>
+          <div className='isFirstIngo_outContainer'>
+            <p className='isFirstIngo_title'>모바일로 접속해 주세요.</p>
+            <p className='isFirstIngo_p'>PL@TER는 모바일에 최적화 되어 있어요.</p>
+            <p className='isFirstIngo_p'>PC 등으로 접속 시 오류가 발생할 수 있어요.</p>
+            <div className='isFirstIngo_button_confirm' onClick={() => { dispatch({ type: 'CHANGE_ISFIRSTINFO', data: !isFirstInfo }); }}>확인</div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
+
+  useEffect(() => {
+    // resize
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+    // Check user device.
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    } else {
+      dispatch({ type: 'CHANGE_ISFIRSTINFO', data: !isFirstInfo });
+    };
+    if (userID === null) {
+      navigater('/login');
+    } else {
+    };
+  }, [navigater, userID])
+
+  return (
+    <React.Fragment>
+      <div className='all_background_img'></div>
+      <section className='section_shootingStar'>
+        <span className='shootingStar'></span>
+        <span className='shootingStar'></span>
+        <span className='shootingStar'></span>
+        <span className='shootingStar'></span>
+      </section>
+      <FirstInfo></FirstInfo>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/*" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/redirect" element={<Redirect />}></Route>
+        <Route path="/redirect2" element={<Redirect2 />}></Route>
+        <Route path="/send" element={<Send />}></Route>
+      </Routes>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ WebContent/web/main/src/components/Redirect.js
@@ -0,0 +1,139 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import './Redirect.css';
+
+function Redirect() {
+    const navigater = useNavigate();
+    const dispatch = useDispatch();
+    const urlParams = new URL(window.location.href).searchParams;
+    const name = urlParams.get('code');
+    const urlParamsErro = new URL(window.location.href).searchParams;
+    const nameErro = urlParamsErro.get('error_description');
+
+    // function requestToken() {
+    //     const body = {
+    //         grant_type: "authorization_code",
+    //         client_id: process.env.REACT_APP_REST_API_KEY,
+    //         redirect_uri: "",
+    //         code: name
+    //     };
+    //     const queryStringBody = Object.keys(body)
+    //         .map(k => encodeURIComponent(k) + "=" + encodeURI(body[k]))
+    //         .join("&");
+    //     fetch("https://kauth.kakao.com/oauth/token", {
+    //         method: "POST",
+    //         headers: {
+    //             'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+    //         },
+    //         body: queryStringBody
+    //     })
+    //         .then(res => res.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             console.log(data.access_token);
+    //             usertoken = data.access_token;
+    //         });
+    // };
+
+    function RequestUserData(userId) {
+        fetch('https://plater.kr/api/member/' + userId, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then((userData) => {
+                console.log(userData);
+                dispatch({ type: 'CHANGE_USERNICKNAME', data: String(userData.userNickName) });
+                dispatch({ type: 'CHANGE_OPENDATE', data: Number(userData.userOpenDate) });
+            })
+            .catch((userDate_error) => {
+                console.log(userDate_error);
+                alert('정상적으로 사용자 데이터를 응답 받지 못했습니다. 다시 로그인 해주세요.');
+                dispatch({ type: 'CHANGE_USERID', data: null });
+                navigater('/login');
+            });
+    };
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (nameErro === 'User denied access') {
+                alert('로그인에 실패하였습니다.');
+                dispatch({ type: 'CHANGE_USERID', data: null });
+                navigater('/login');
+            };
+            const code = { code: name };
+            console.log(code);
+            const queryStringBody = Object.keys(code)
+                .map(k => encodeURIComponent(k) + "=" + encodeURI(code[k]))
+                .join("&");
+            fetch('https://plater.kr/api/kakao/', {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                },
+                body: queryStringBody
+            })
+                .then(res => res.json())
+                .then((data) => {
+                    console.log('암호화된 ID: ' + data);
+                    fetch('https://plater.kr/api/member/', {
+                        method: 'POST',
+                        mode: 'cors',
+                        cache: 'no-cache',
+                        credentials: 'same-origin',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    })
+                        .then(plus_res => plus_res.json())
+                        .then((plus_data) => {
+                            console.log('비암호화된 ID: ' + plus_data);
+                            dispatch({ type: 'CHANGE_USERID', data: plus_data });
+                            RequestUserData(plus_data);
+                            navigater('/main');
+                        })
+                        .catch((plus_error) => {
+                            console.log('비암호화된 ID 오류: ' + plus_error);
+                            alert('서버가 불안정 하여 비암호화된 사용자 ID를 받아오지 못했습니다.');
+                            dispatch({ type: 'CHANGE_USERID', data: null });
+                            navigater('/login');
+                        })
+                })
+                .catch((error) => {
+                    console.log('암호화된 ID 오류: ' + error);
+                    alert('서버가 불안정 하여 로그인에 실패했습니다.');
+                    dispatch({ type: 'CHANGE_USERID', data: null });
+                    navigater('/login');
+                });
+        }, 500);
+    }, []);
+    // }, [name, nameErro, navigater, dispatch]);
+
+    return (
+        <React.Fragment>
+            <div className='redirect_outContainer'>
+                <section className='section_shootingStar'>
+                    <span className='shootingStar'></span>
+                    <span className='shootingStar'></span>
+                    <span className='shootingStar'></span>
+                    <span className='shootingStar'></span>
+                </section>
+                <div className='redirect_gif'></div>
+                <h3 className='redirect_h3'>로그인 중입니다...</h3>
+                <h4 className='redirect_h4'>[리빙포인트] 달에는 토끼가 떡을 만들고 있다.</h4>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Redirect;
