856a11e5b899e021d521dd71fc5fc12b55d63cfa
Angelo <blue_notion@naver.com>
2023-03-22 21:56:33 +0900

    update_2023-03-22_v1
    1. main component에 편지 확인 기능 구현
    2. send component에 api/letter 적용 / 단, 아직 정상 작동하지 않음.

--- main/src/components/Send.js
+++ main/src/components/Send.js
@@ -430,76 +430,53 @@ function Send() {
     };
 
     function SendingPage() {
-        let newStickerArray = [...stickerArray];
-        for (let i = 0; i < newStickerArray.length; i++) {
-            delete newStickerArray[i].id
-        };
-        let letterData = {
-            'letter': {
-                'letterId': '',
-                'userId': String(shareUserID),
-                'letterContent': String(text),
-                'letterFont': String(styleLetter['fontFamily']),
-                'letterTextAlign': String(styleLetter['textAlign']),
-                'letterFontSize': String(styleLetter['fontSize']),
-                'letterFontColor': String(styleLetter['color']),
-                'letterPaper': String(styleLetter['backgroundImage']),
-                'letterIcon': String(stamp),
-                'letterWriter': String(author)
-            },
-            'sticker': {
-                stickerArray
-            }
-        };
 
-        // function sendLetterFunc() {
-        //     fetch('https://plater.kr/api/letter', {
-        //         method: 'POST',
-        //         mode: 'cors',
-        //         cache: 'no-cache',
-        //         credentials: 'same-origin',
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         body: JSON.stringify(letterData)
-        //     })
-        //         .then(res => res.json())
-        //         .then((data) => {
-        //             console.log(data);
-        //         })
-        //         .catch((error) => {
-        //             console.log(error);
-        //             alert('서버가 불안정 하여 편지가 정상적으로 발송되지 않았습니다. 다시 시도해주세요.');
-        //             dispatch({ type: 'CHANGE_ISSENDINGPAGE', data: !isSendingPage });
-        //             dispatch({ type: 'CHANGE_ISPRELETTERBOX', data: !isPreLetterBox });
-        //             dispatch({ type: 'CHANGE_ISSENDMAIN', data: !isSendMain });
-        //         });
-        // };
+        function sendLetterFunc(props) {
+            fetch('https://plater.kr/api/letter', {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(props)
+            })
+                .then(res => res.json())
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    alert('서버가 불안정 하여 편지가 정상적으로 발송되지 않았습니다. 다시 시도해주세요.');
+                    dispatch({ type: 'CHANGE_ISSENDINGPAGE', data: !isSendingPage });
+                    dispatch({ type: 'CHANGE_ISPRELETTERBOX', data: !isPreLetterBox });
+                    dispatch({ type: 'CHANGE_ISSENDMAIN', data: !isSendMain });
+                });
+        };
 
         function checkLetterData() {
-            let sendLetterData = {
-                'text': text,
-                'font': styleLetter['fontFamily'],
-                'align': styleLetter['textAlign'],
-                'size': styleLetter['fontSize'],
-                'color': styleLetter['color'],
-                'paper': styleLetter['backgroundImage'],
-                'icon': stamp,
-                'sticker': stickerArray,
-                'author': author
+            let postSticker = [...stickerArray];
+            for (let i = 0; i < postSticker.length; i++) {
+                delete postSticker[i].id
             };
-            console.log(sendLetterData);
+            let sticker = Object.assign({},postSticker);
+            let letterData = {
+                'letter': {
+                    'userId': String(shareUserID),
+                    'letterContent': String(text),
+                    'letterFont': String(styleLetter['fontFamily']),
+                    'letterTextAlign': String(styleLetter['textAlign']),
+                    'letterFontSize': String(styleLetter['fontSize']),
+                    'letterFontColor': String(styleLetter['color']),
+                    'letterPaper': String(styleLetter['backgroundImage']),
+                    'letterWriter': String(author),
+                    'letterIcon': String(stamp)                 
+                },
+                sticker
+            };
             console.log(letterData);
-            alert(`text : ${text}
-             / font : ${styleLetter['fontFamily']} 
-             / align : ${styleLetter['textAlign']} 
-             / size : ${styleLetter['fontSize']} 
-             / color : ${styleLetter['color']}
-             / paper : ${styleLetter['backgroundImage']}
-             / icon : ${stamp}
-             / sticker : ${stickerArray}
-             / author : ${author}
-            `);
+            sendLetterFunc(letterData);
             dispatch({ type: 'CHANGE_ISSENDINGEND', data: !isSendingEnd });
             dispatch({ type: 'CHANGE_ISSENDINGPAGE', data: !isSendingPage });
         };
