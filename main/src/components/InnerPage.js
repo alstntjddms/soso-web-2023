942763da2e4773f87461b1080fef9e6535cdd8fb
Angelo <blue_notion@naver.com>
2023-05-16 20:55:34 +0900

    update_2023-05-16_v2

--- main/src/components/InnerPage.js
+++ main/src/components/InnerPage.js
@@ -787,7 +787,7 @@ function InnerPage() {
                 scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
                 ins.setAttribute('data-ad-width', '320');
                 ins.setAttribute('data-ad-height', '100');
-                ins.setAttribute('data-ad-unit', 'DAN-ynvjcyFHlXsbbDzL');
+                ins.setAttribute('data-ad-unit', 'DAN-wwtMTOs6oLrop9iK');
                 document.querySelector('.adfit').appendChild(ins);
                 document.querySelector('.adfit').appendChild(scr);
             }, [])
