27cc0fa44b16c4d6d6a4b9cf30c161a09b072fb8
Angelo <blue_notion@naver.com>
2023-06-01 20:20:06 +0900

    update_2023-06-01_v1
    html2canvas 기능 확인

--- refactoring/src/App.js
+++ refactoring/src/App.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect, useRef } from 'react'
+import html2canvas from 'html2canvas';
 import './App.css';
 
 function App() {
 
+  function capture() {
+    html2canvas(document.querySelector('.capture')).then(canvas => {
+      onSavePic(canvas.toDataURL('image/png'), 'image-download.png');
+    });
+  };
+
+  function onSavePic(url, fileName) {
+    let link = document.createElement('a');
+    document.body.appendChild(link);
+    link.href = url;
+    link.download = fileName;
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <React.Fragment>
+      <div className='capture' onClick={capture}>
+        <div className='inner'>
+          <div className='sticker'></div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
